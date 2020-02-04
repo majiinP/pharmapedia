60c6dfa6c097502864f52af72affebebeacc9f92
Antonie Pescaru <68.pierre.pescaru@gmail.com>
2020-02-04 09:36:47 -0500

    Correct links
    Updated index css, js and external files with correct links

--- index.php
+++ index.php
@@ -20,30 +20,30 @@
          <script src="https://use.fontawesome.com/dc8cb5d611.js"></script>
          <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-         <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/font-awesome.min.css">
+         <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
 
         <!-- ==================== JS INTERNAL  =============================== -->
-        <script type="text/javascript" src="../medication-encyclopedia/js/medication-appear.js"></script>
-        <script type="text/javascript" src="../medication-encyclopedia/js/reponsive-menu.js"></script>
-        <script type="text/javascript" src="../medication-encyclopedia/js/cookie-banner.js"></script>
+        <script type="text/javascript" src="../js/medication-appear.js"></script>
+        <script type="text/javascript" src="../js/reponsive-menu.js"></script>
+        <script type="text/javascript" src="../js/cookie-banner.js"></script>
 
         <!-- ==================== CSS INTERNAL =============================== -->
         <!-- NAV-MENU STYLE SHEET -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/nav-menu.css"/>
+        <link rel="stylesheet" type="text/css" href="../css/nav-menu.css"/>
         <!-- COOKIE BANNER STYLE SHEET -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/cookie-banner.css">
+        <link rel="stylesheet" type="text/css" href="../css/cookie-banner.css">
         <!-- SEARCHBAR STYLE SHEET -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/SearchBar.css">
+        <link rel="stylesheet" type="text/css" href="../css/SearchBar.css">
         <!-- BUTTONS STYLE -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/buttons.css">
+        <link rel="stylesheet" type="text/css" href="../css/buttons.css">
          <!-- OVERLAY STYLE SHEET -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/overlay.css">
+        <link rel="stylesheet" type="text/css" href="../css/overlay.css">
          <!-- GENERAL STYLE SHEET -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/GeneralStylesheet.css">
+        <link rel="stylesheet" type="text/css" href="../css/GeneralStylesheet.css">
         <!-- FOOTER STYLE SHEET -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/footer.css">
+        <link rel="stylesheet" type="text/css" href="../css/footer.css">
          <!-- MEDIA-QUERIES -->
-        <link rel="stylesheet" type="text/css" href="../medication-encyclopedia/css/media-queries.css">
+        <link rel="stylesheet" type="text/css" href="../css/media-queries.css">
 
     </head>
 
